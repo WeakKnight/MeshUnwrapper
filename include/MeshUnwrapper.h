94a3455fbf5ebf652a2d01cd6ddb4f7aadf49a95
Tianyu Li <ltyucb@gmail.com>
2021-11-06 04:29:29 +0800

    WIP

--- include/MeshUnwrapper.h
+++ include/MeshUnwrapper.h
@@ -6,7 +6,7 @@ extern "C"
 {
     MU_API void     mu_init(int maxAtlasSize, float texelDensity);
     MU_API void     mu_set_positions(void* position, int itemSize, int itemCount);
-    MU_API void     mu_set_indices(void* indicies, int itemSize, int itemCount);
+    MU_API void     mu_set_indices(void* indicies, int itemSize, int itemCount, bool isUInt32);
     MU_API void     mu_build();
     MU_API void*    mu_get_unwrapped_uvs();
     MU_API int      mu_get_atlas_width();
--- src/MeshUnwrapper.cpp
+++ src/MeshUnwrapper.cpp
@@ -1,19 +1,32 @@
 #include <iostream>
 #include "xatlas.h"
 
+xatlas::Atlas* s_atlas = nullptr;
+xatlas::PackOptions s_packOptions;
+xatlas::MeshDecl s_meshDesc;
+int s_maxAtlasSize;
+
 extern "C"
 {
     void mu_init(int maxAtlasSize, float texelDensity)
     {
-
+        if (s_atlas != nullptr)
+        {
+            xatlas::Destroy(s_atlas);
+        }
+        s_atlas = xatlas::Create();
+        s_packOptions.texelsPerUnit = texelDensity;
+        s_maxAtlasSize = maxAtlasSize;
     }
 
     void mu_set_positions(void* position, int itemSize, int itemCount)
     {
-
+        s_meshDesc.vertexPositionData = position;
+        s_meshDesc.vertexPositionStride = itemSize * 3;
+        s_meshDesc.vertexCount = itemCount / 3;
     }
 
-    void mu_set_indices(void* indicies, int itemSize, int itemCount)
+    void mu_set_indices(void* indicies, int itemSize, int itemCount, bool isUInt32)
     {
 
     }
