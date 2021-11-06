cb6632905fedfb28c688a03580f4a2c3c53e961d
Tianyu Li <ltyucb@gmail.com>
2021-11-07 05:24:55 +0800

    WIP

--- include/MeshUnwrapper.h
+++ include/MeshUnwrapper.h
@@ -1,14 +1,27 @@
 #pragma once
+#include <stdint.h>
 
 #define MU_API __declspec(dllexport) 
 
 extern "C" 
 {
-    MU_API void     mu_init(int maxAtlasSize, float texelDensity);
-    MU_API void     mu_set_positions(void* position, int itemSize, int itemCount);
-    MU_API void     mu_set_indices(void* indicies, int itemSize, int itemCount, bool isUInt32);
-    MU_API void     mu_build();
-    MU_API void*    mu_get_unwrapped_uvs();
-    MU_API int      mu_get_atlas_width();
-    MU_API int      mu_get_atlas_height();
+    MU_API void         mu_init(uint32_t maxAtlasSize, float texelDensity);
+    MU_API void         mu_set_positions(void* position, uint32_t itemSize, uint32_t itemCount);
+    MU_API void         mu_set_indices(void* indicies, uint32_t itemSize, uint32_t itemCount);
+    MU_API void         mu_build();
+
+    MU_API uint32_t     mu_get_atlas_width();
+    MU_API uint32_t     mu_get_atlas_height();
+
+    /*Per Vertex Data*/
+    MU_API float        mu_get_unwrapped_mesh_u(uint32_t i);
+    MU_API float        mu_get_unwrapped_mesh_v(uint32_t i);
+    MU_API uint32_t     mu_get_unwrapped_mesh_original_index(uint32_t i);
+    MU_API uint32_t     mu_get_unwrapped_mesh_vertex_count();
+
+    /*Per Index Data*/
+    MU_API uint32_t     mu_get_unwrapped_mesh_index(uint32_t i);
+    MU_API uint32_t     mu_get_unwrapped_mesh_index_count();
+
+    MU_API void         mu_free();
 }
--- src/MeshUnwrapper.cpp
+++ src/MeshUnwrapper.cpp
@@ -1,48 +1,96 @@
 #include <iostream>
 #include "xatlas.h"
+#include "MeshUnwrapper.h"
 
 xatlas::Atlas* s_atlas = nullptr;
 xatlas::PackOptions s_packOptions;
 xatlas::MeshDecl s_meshDesc;
-int s_maxAtlasSize;
+uint32_t s_maxAtlasSize;
 
 extern "C"
 {
-    void mu_init(int maxAtlasSize, float texelDensity)
+    void mu_init(uint32_t maxAtlasSize, float texelDensity)
     {
         if (s_atlas != nullptr)
         {
             xatlas::Destroy(s_atlas);
+            s_atlas = nullptr;
         }
         s_atlas = xatlas::Create();
         s_packOptions.texelsPerUnit = texelDensity;
         s_maxAtlasSize = maxAtlasSize;
     }
 
-    void mu_set_positions(void* position, int itemSize, int itemCount)
+    void mu_set_positions(void* position, uint32_t itemSize, uint32_t itemCount)
     {
         s_meshDesc.vertexPositionData = position;
         s_meshDesc.vertexPositionStride = itemSize * 3;
         s_meshDesc.vertexCount = itemCount / 3;
     }
 
-    void mu_set_indices(void* indicies, int itemSize, int itemCount, bool isUInt32)
+    void mu_set_indices(void* indicies, uint32_t itemSize, uint32_t itemCount)
     {
-
-    }
-
-    void mu_set_texel_density(float density)
-    {
-
+        s_meshDesc.indexData = indicies;
+        s_meshDesc.indexFormat = (itemSize == 2? xatlas::IndexFormat::UInt16: xatlas::IndexFormat::UInt32);
+        s_meshDesc.indexCount = itemCount;
     }
 
     void mu_build()
     {
-
+        xatlas::AddMesh(s_atlas, s_meshDesc);
+        xatlas::ComputeCharts(s_atlas);
+        xatlas::PackCharts(s_atlas, s_packOptions);
+        if (s_atlas->width > s_maxAtlasSize || s_atlas->height > s_maxAtlasSize)
+        {
+            s_packOptions.texelsPerUnit = 0;
+            s_packOptions.resolution = s_maxAtlasSize;
+            xatlas::PackCharts(s_atlas, s_packOptions);
+        }
     }
 
-    void* mu_get_unwrapped_uvs()
+    uint32_t mu_get_atlas_width()
     {
-        return nullptr;
+        return s_atlas->width;
+    }
+
+    uint32_t mu_get_atlas_height()
+    {
+        return s_atlas->height;
+    }
+
+    float mu_get_unwrapped_mesh_u(uint32_t i)
+    {
+        return s_atlas->meshes[0].vertexArray[i].uv[0] / s_atlas->width;
+    }
+
+    float mu_get_unwrapped_mesh_v(uint32_t i)
+    {
+        return s_atlas->meshes[0].vertexArray[i].uv[0] / s_atlas->height;
+    }
+
+    uint32_t mu_get_unwrapped_mesh_original_index(uint32_t i)
+    {
+        return s_atlas->meshes[0].vertexArray[i].xref;
+    }
+
+    uint32_t mu_get_unwrapped_mesh_vertex_count()
+    {
+        return s_atlas->meshes[0].vertexCount;
+    }
+
+    uint32_t mu_get_unwrapped_mesh_index(uint32_t i)
+    {
+        return s_atlas->meshes[0].indexArray[i];
+    }
+
+    uint32_t mu_get_unwrapped_mesh_index_count()
+    {
+        return s_atlas->meshes[0].indexCount;
+    }
+
+    void mu_free()
+    {
+        xatlas::Destroy(s_atlas);
+        s_atlas = nullptr;
     }
 }
